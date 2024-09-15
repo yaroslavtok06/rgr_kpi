552e71838cdbf65cc6a762cae977bffbee3b6e0d
Yaroslav <yaroslavtok.work@gmail.com>
2024-09-15 23:27:48 +0300

    Add new code

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
 import json
 import requests
 
-
+#API air pollution
 api_key = "988e37e63684135f6d0f6e471e066257"
-#London
-lat = 37.1289771
-lon = -84.0832646
+
+#get city from api
+city = "Paris"
+
+city_request = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={city}&appid={api_key}")
+if city_request.status_code == 200:
+    data_city = city_request.json()
+
+    #write in file data.json
+    with open('city.json', 'w', encoding='utf-8') as file:
+        json.dump(data_city, file, ensure_ascii=False, indent=4)
+
+    with open('city.json', 'r', encoding='utf-8') as file:
+        data_city = json.load(file)
+
+    contry = data_city[0]['name']
+    lat = data_city[0]['lat']
+    lon = data_city[0]['lon']
+
+    print(contry)
+    print(lat)
+    print(lon)
+    
+else:
+    print(city_request.status_code)
+
+#status pollution
 
 response = requests.get(f"http://api.openweathermap.org/data/2.5/air_pollution?lat={lat}&lon={lon}&appid={api_key}")
 if response.status_code == 200:
